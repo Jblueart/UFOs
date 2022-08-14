d543fd4926be33af7893ded66f8b3aa64116212a
Jblueart <104408782+Jblueart@users.noreply.github.com>
2022-08-14 15:58:25 -0600

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,33 @@
 // from data.js
 var tableData = data;
-
+var filter_btn = d3.select("#filter-btn");
 // get table references
 var tbody = d3.select("tbody");
+var reset_table = d3.select("#reset_table");
+filter_btn.on("click", function(){
+  d3.event.preventDefault();
+  tbody.selectAll("td").remove();
+
+  var datetime = d3.select("#datetime").property("value");
+  var city = d3.select("#city").property("value");
+  var state = d3.select("#state").property("value");
+  var country = d3.select("#country").property("value");
+  var shape = d3.select("#shape").property("value");
+  var filter = "var filtered_table = tableData";
+
+  if(datetime) { filter = filter.concat(".filter(sighting => sighting.datetime === datetime)"); }
+  if(city) { filter = filter.concat(".filter(sighting => sighting.city === city)"); }
+  if(state) { filter = filter.concat(".filter(sighting => sighting.state === state)"); }
+  if(country) { filter = filter.concat(".filter(sighting => sighting.country === country)"); }
+  if(shape) { filter = filter.concat(".filter(sighting => sighting.shape === shape)"); }
+
+    console.log(filter);
+    eval(filter);
+
+    
+
+});
+
 
 function buildTable(data) {
   // First, clear out any existing data
